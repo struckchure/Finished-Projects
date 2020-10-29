bdb35513335f1876d79aa625e1d10cb694b70cf4
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2020-10-29 15:19:59 +0100

    entry point

---
+++ DiamondRubyQuiz/main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from Quiz.main import QuizHome as Window
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationName("QUIZ APP")
+    window = Window()
+    window.show()
+    app.exec_()
